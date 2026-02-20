509820e67a2e1c05a6b0f55f61afc6e80b8d4e4a
Kadir-Bo <kadir-bo@outlook.com>
2026-02-20 09:11:38 +0100

    Replaced Logo Image with Logo Button Comopnent

---
+++ components/buttons/LogoButton.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+export default function LogoButton() {
+  return (
+    <Link
+      href={"/"}
+      className="text-2xl font-serif font-light tracking-[-1.5px] py-3 px-2 "
+    >
+      openchat
+    </Link>
+  );
+}

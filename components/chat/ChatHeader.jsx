5bc439110b911b128b5213d1af5dbecfaa54861b
Kadir-Bo <kadir-bo@outlook.com>
2026-02-26 10:17:41 +0100

    feat(Chat Header):
    Removed Logo from Chat Header;
    Adjusted Header Padding;

--- components/chat/ChatHeader.jsx
+++ components/chat/ChatHeader.jsx
@@ -5,7 +5,7 @@ import { LogoButton, PrimaryButton } from "@/components";
 export default function ChatHeader({ handleToggleSidebar }) {
   return (
     <div className="w-full bg-neutral-900">
-      <div className="max-w-550 mx-auto flex justify-between items-center px-2 py-2 h-max">
+      <div className="max-w-550 mx-auto flex justify-between items-center px-2 py-2 md:py-1 h-max">
         <div className="flex-1">
           <button
             className="outline-none p-3 h-full md:hidden"
@@ -15,17 +15,15 @@ export default function ChatHeader({ handleToggleSidebar }) {
             <Menu size={20} />
           </button>
         </div>
-        <div className="flex items-center justify-center flex-1">
-          <LogoButton />
-        </div>
+        <div className="flex items-center justify-center flex-1"></div>
         <div className="flex flex-1 justify-end">
           <PrimaryButton
-            className="p-3 m-0 border-none w-max"
+            className="p-2 m-0 border-none w-max"
             tooltip={"New Chat"}
             tooltipPosition="left"
             href={"/chat"}
           >
-            <MessageSquare size={20} />
+            <MessageSquare size={18} />
           </PrimaryButton>
         </div>
       </div>

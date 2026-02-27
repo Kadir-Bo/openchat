8b475d565283b40b9a0429c4f67d886e72e70e68
Kadir-Bo <kadir-bo@outlook.com>
2026-02-27 11:53:45 +0100

    feat(settings): Sticky Navigation for Desktop

--- app/(chat)/settings/layout.jsx
+++ app/(chat)/settings/layout.jsx
@@ -15,7 +15,7 @@ export default function SettingsLayout({ children }) {
     <div className={`h-[calc(100dvh-48px)] overflow-y-scroll px-4`}>
       <div className="max-w-220 mx-auto flex flex-col md:flex-row gap-6 items-start">
         {/* Sidebar Navigation */}
-        <aside className="w-full md:w-48 fixed md:relative left-0 md:top-0 z-99 shrink-0 px-5 py-2 md:py-0 md:px-0 bg-neutral-900 border-t md:border-none border-neutral-800 md:bg-transparent">
+        <aside className="w-full md:w-48 fixed md:sticky left-0 md:top-16 z-99 shrink-0 px-5 py-2 md:py-0 md:px-0 bg-neutral-900 border-t md:border-none border-neutral-800 md:bg-transparent">
           <nav aria-label="Settings navigation">
             <ul className="space-y-1 flex flex-row md:flex-col justify-center">
               {tabs.map((tab) => {
@@ -38,7 +38,7 @@ export default function SettingsLayout({ children }) {
         </aside>
 
         {/* Main Content */}
-        <main className="mt-16 pt-8 pb-16 min-h-dvh">{children}</main>
+        <main className="mt-16 pt-8 md:pt-0 pb-16 min-h-dvh">{children}</main>
       </div>
     </div>
   );
